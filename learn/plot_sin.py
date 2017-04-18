5ff7c5dc78877762dd53b0d153745d0681a33e58
JasonLearning <616273218@qq.com>
2017-04-18 16:04:27 +0800

    [2017.4.18] [add svm train]

--- learn/plot_sin.py
+++ learn/plot_sin.py
@@ -11,6 +11,7 @@ from mongoengine import *
 
 
 class DataCollection(Document):
+    id_num = IntField()
     facility_type = IntField()
     direction = IntField()
     p = ListField()
@@ -24,14 +25,15 @@ class DataCollection(Document):
     second_judge_artificial = IntField()
 
 
-class ShopListJobCollectionClass:
+class DataCollectionClass:
     def __init__(self):
         self.db_name = "casco"
         connect(self.db_name)
 
     @staticmethod
-    def insert(p, a, b, c, f, d, man, f1, f2, s1, s2):
+    def insert(id, p, a, b, c, f, d, man, f1, f2, s1, s2):
         DataCollection(
+            id_num=id,
             facility_type=f,
             direction=d,
             p=p,
@@ -49,7 +51,7 @@ class ShopListJobCollectionClass:
 def deal_csv():
     file = 'casco_train.csv'
     total = pd.read_csv(file)
-    for i in np.arange(180635, 190642, 1):
+    for i in range(190642):
         sd = total['switch_data'][i]
         full_string = sd[2:-1].split(',')
         full_list = []
@@ -61,12 +63,12 @@ def deal_csv():
         c = full_list[3000:]
         man = total['isManmade'][i]
         f = total['facility_type'][i]
-        d = total['direction'][i]
+        d = int(total['direction'][i])
         f1 = total['first_judge_machine'][i]
         f2 = total['first_judge_artificial'][i]
         s1 = total['second_judge_machine'][i]
         s2 = total['second_judge_artificial'][i]
-        bsbsbs = ShopListJobCollectionClass()
+        bsbsbs = DataCollectionClass()
         bsbsbs.insert(p, a, b, c, f, d, man, f1, f2, s1, s2)
 
 
@@ -81,27 +83,30 @@ def draw_sin():
     plt.show()
 
 
-def draw_wave(switch_data, num):
-    color = ['black', 'blue', 'red']
+def draw_wave(switch_data, right=True):
+    if right:
+        color = 'black'
+    else:
+        color = 'red'
     t = np.arange(0, 10, 0.01)
     data = switch_data
-    plt.plot(t, data, color[num])
+    plt.plot(t, data, color)
 
 
 def find_and_show():
     db_name = "casco"
     connect(db_name)
-    right = DataCollection.objects(first_judge_machine=0)[:100]
+    right = DataCollection.objects(first_judge_machine=0)[:1000]
     for item in right:
-        draw_wave(item.c, 1)
-    wrong = DataCollection.objects(first_judge_machine=1)[:100]
+        draw_wave(item.p)
+    wrong = DataCollection.objects(first_judge_machine=1)[:300]
     for item in wrong:
-        draw_wave(item.c, 2)
+        draw_wave(item.p, False)
 
     plt.show()
 
 
 if __name__ == '__main__':
     # draw_sin()
-    deal_csv()
-    # find_and_show()
+    # deal_csv()
+    find_and_show()
